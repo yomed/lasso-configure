21a82275a7cdb08151d75762023f5a2ce084d98f
Yoni Medoff <yonimedoff@gmail.com>
2016-07-05 21:38:47 -0700

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+var isProduction = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test';
+
+require('lasso').configure({
+    plugins: [
+        'lasso-marko',
+        {
+            plugin: 'lasso-less',
+            config: {
+                lessConfig: {
+                    strictMath: true,
+                    strictUnits: true
+                }
+            }
+        },
+        {
+            plugin: 'lasso-clean-css',
+            config: {
+                enabled: isProduction
+            }
+        },
+        {
+            plugin: 'lasso-autoprefixer',
+            config: {
+                browsers: '> 1%'
+            }
+        }
+    ],
+    outputDir: 'static',
+    fingerprintsEnabled: isProduction,
+    minify: isProduction,
+    resolveCssUrls: true,
+    bundlingEnabled: isProduction
+});
