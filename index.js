501f8be22dfe17237b3a0d9071b8d316e29b9a7e
Yoni Medoff <yonimedoff@gmail.com>
2017-05-27 10:41:20 -0700

    add option for separate jquery bundle

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
 'use strict';
 
+var lasso = require('lasso');
+
 var isProduction = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test';
 
-require('lasso').configure({
-    plugins: [
-        'lasso-marko',
-        {
-            plugin: 'lasso-less',
-            config: {
-                lessConfig: {
-                    strictMath: true,
-                    strictUnits: true
+function run(hasJqueryBundle) {
+    var bundles;
+
+    if (hasJqueryBundle) {
+        bundles = [
+            {
+                name: 'jquery',
+                dependencies: [
+                    'jquery/dist/jquery.min.js'
+                ]
+            }
+        ]
+    };
+
+    lasso.configure({
+        plugins: [
+            'lasso-marko',
+            {
+                plugin: 'lasso-less',
+                config: {
+                    lessConfig: {
+                        strictMath: true,
+                        strictUnits: true
+                    }
+                }
+            },
+            {
+                plugin: 'lasso-clean-css',
+                config: {
+                    enabled: isProduction
+                }
+            },
+            {
+                plugin: 'lasso-autoprefixer',
+                config: {
+                    browsers: '> 1%'
                 }
             }
-        },
-        {
-            plugin: 'lasso-clean-css',
-            config: {
-                enabled: isProduction
-            }
-        },
-        {
-            plugin: 'lasso-autoprefixer',
-            config: {
-                browsers: '> 1%'
-            }
-        }
-    ],
-    outputDir: 'static',
-    fingerprintsEnabled: isProduction,
-    minify: isProduction,
-    resolveCssUrls: true,
-    bundlingEnabled: isProduction
-});
+        ],
+        outputDir: 'static',
+        fingerprintsEnabled: isProduction,
+        minify: isProduction,
+        resolveCssUrls: true,
+        bundlingEnabled: isProduction,
+        bundles: bundles
+    });
+}
+
+module.exports = run;
